ec06ec9d871d3173752e0ba2e38791b2af8c5269
TechcatGato #2750 <i.am.yoav@gmail.com>
2026-03-17 10:24:06 +0200

    pb formatting

--- pybricks/experimental/pb_module_experimental.c
+++ pybricks/experimental/pb_module_experimental.c
@@ -15,14 +15,14 @@
     #define ACCEL_RAM __attribute__((section(".data"), noinline))
 #else
     #define IS_CORTEX_M 0
-    #define ACCEL_RAM 
+    #define ACCEL_RAM
 #endif
 
 // Constants
-static const float PI_F          = 3.141592653589793f;
-static const float TWO_PI_F      = 6.283185307179586f;
-static const float HALF_PI_F     = 1.570796326794896f;
-static const float INV_TWO_PI_F  = 0.159154943091895f;
+static const float PI_F = 3.141592653589793f;
+static const float TWO_PI_F = 6.283185307179586f;
+static const float HALF_PI_F = 1.570796326794896f;
+static const float INV_TWO_PI_F = 0.159154943091895f;
 
 // -----------------------------------------------------------------------------
 // Core Math Engine (Lasse Schlör Absolute Error Optimized)
@@ -31,17 +31,20 @@ ACCEL_RAM static float fast_sin_internal(float theta) {
     float x = theta * INV_TWO_PI_F;
     x = theta - (float)((int)(x + (x > 0 ? 0.5f : -0.5f))) * TWO_PI_F;
 
-    if (x > HALF_PI_F) { x = PI_F - x; }
-    else if (x < -HALF_PI_F) { x = -PI_F - x; }
+    if (x > HALF_PI_F) {
+        x = PI_F - x;
+    } else if (x < -HALF_PI_F) {
+        x = -PI_F - x;
+    }
 
     float x2 = x * x;
     #if IS_CORTEX_M
-        float res = -0.0001848814f; 
-        res = 0.0083119000f + (x2 * res);
-        res = -0.1666555409f + (x2 * res);
-        return x * (0.9999990609f + (x2 * res));
+    float res = -0.0001848814f;
+    res = 0.0083119000f + (x2 * res);
+    res = -0.1666555409f + (x2 * res);
+    return x * (0.9999990609f + (x2 * res));
     #else
-        return x * (0.9999990609f + x2 * (-0.1666555409f + x2 * (0.0083119000f + x2 * -0.0001848814f)));
+    return x * (0.9999990609f + x2 * (-0.1666555409f + x2 * (0.0083119000f + x2 * -0.0001848814f)));
     #endif
 }
 
@@ -67,8 +70,8 @@ static mp_obj_t experimental_odometry_benchmark(size_t n_args, const mp_obj_t *a
     mp_obj_t left_angle_func = args[3];
     mp_obj_t db_angle_func = args[4];
 
-    float deg_to_mm = wheel_circ / 720.0f; 
-    
+    float deg_to_mm = wheel_circ / 720.0f;
+
     // State variables
     float robot_x = 0.0f, robot_y = 0.0f;
     float last_linear = 0.0f;
@@ -78,10 +81,10 @@ static mp_obj_t experimental_odometry_benchmark(size_t n_args, const mp_obj_t *a
 
     // 2. The Core High-Speed C Loop
     for (int i = 0; i < num_iters; i++) {
-        
+
         // Execute the exact same hardware getters as your Python script
         float right_angle = get_float_from_obj(mp_call_function_0(right_angle_func));
-        float left_angle  = get_float_from_obj(mp_call_function_0(left_angle_func));
+        float left_angle = get_float_from_obj(mp_call_function_0(left_angle_func));
         float robot_heading = get_float_from_obj(mp_call_function_0(db_angle_func));
 
         // Exactly mirroring your Python math logic
@@ -95,13 +98,15 @@ static mp_obj_t experimental_odometry_benchmark(size_t n_args, const mp_obj_t *a
         if (fabsf(linear) > 0.0f) {
             float avg_heading_deg = last_heading - (heading_difference / 2.0f);
             float avg_heading_rad = avg_heading_deg * 0.0174532925f; // DEG_TO_RAD
-            
+
             robot_x += linear * fast_sin_internal(avg_heading_rad + HALF_PI_F); // cos
             robot_y += linear * fast_sin_internal(avg_heading_rad); // sin
         }
 
         // Safety: Prevent watchdog timeouts during massive loops
-        if ((i % 1000) == 0) { mp_handle_pending(true); }
+        if ((i % 1000) == 0) {
+            mp_handle_pending(true);
+        }
     }
 
     uint32_t duration_ms = mp_hal_ticks_ms() - start_time;
@@ -122,8 +127,8 @@ static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(experimental_odometry_benchmark_obj, 
 
 // Module Globals
 static const mp_rom_map_elem_t experimental_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__),             MP_ROM_QSTR(MP_QSTR_experimental) },
-    { MP_ROM_QSTR(MP_QSTR_odometry_benchmark),   MP_ROM_PTR(&experimental_odometry_benchmark_obj) },
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_experimental) },
+    { MP_ROM_QSTR(MP_QSTR_odometry_benchmark), MP_ROM_PTR(&experimental_odometry_benchmark_obj) },
 };
 static MP_DEFINE_CONST_DICT(pb_module_experimental_globals, experimental_globals_table);
 
