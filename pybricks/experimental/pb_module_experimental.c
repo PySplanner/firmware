1abc66d410926a75253c11e1fc98b5325bc2a38c
TechcatGato #2750 <i.am.yoav@gmail.com>
2026-03-18 00:34:01 +0200

    please build i need this

--- pybricks/experimental/pb_module_experimental.c
+++ pybricks/experimental/pb_module_experimental.c
@@ -1,10 +1,16 @@
 #include "py/mpconfig.h"
+#include "py/obj.h"
+#include "py/runtime.h"
+#include "py/builtin.h"
 
 #if PYBRICKS_PY_EXPERIMENTAL
 
-#include "py/obj.h"
-#include "py/runtime.h"
 #include "pybricks/experimental/odometry.h"
+
+// Define STATIC if not already defined (usually it is in mpconfig.h)
+#ifndef STATIC
+#define STATIC static
+#endif
 
 // This is the function that Python calls
 STATIC mp_obj_t experimental_odometry_benchmark(size_t n_args, const mp_obj_t *args) {
@@ -16,22 +22,19 @@ STATIC mp_obj_t experimental_odometry_benchmark(size_t n_args, const mp_obj_t *a
 
     return calculate_odometry(num_iters, wheel_circ, axle_track, right_func, left_func);
 }
-// We define it as a MicroPython function object
+// Define as a MicroPython function object
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(experimental_odometry_benchmark_obj, 5, 5, experimental_odometry_benchmark);
 
-// We map the C function to the Python name "odometry_benchmark"
+// Map the C function to the Python name "odometry_benchmark"
 STATIC const mp_rom_map_elem_t experimental_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_odometry_benchmark), MP_ROM_PTR(&experimental_odometry_benchmark_obj) },
 };
 STATIC MP_DEFINE_CONST_DICT(pb_module_experimental_globals, experimental_globals_table);
 
-// This is the structure MicroPython looks for to register the module
+// Structure for MicroPython to register the module
 const mp_obj_module_t pb_module_experimental = {
     .base = { &mp_type_module },
     .globals = (mp_obj_dict_t *)&pb_module_experimental_globals,
 };
-
-// CRITICAL: We do NOT need a MP_REGISTER_MODULE tag here because 
-// the Pybricks build system handles registration via the Makefile/QSTR process.
 
 #endif // PYBRICKS_PY_EXPERIMENTAL
