b334b36373bcc0de04728afe5dce8c5444e31881
TechcatGato #2750 <i.am.yoav@gmail.com>
2026-03-18 21:07:45 +0200

    optimize spike

--- pybricks/experimental/platform_math.h
+++ pybricks/experimental/platform_math.h
@@ -3,32 +3,21 @@
 
 #include <math.h>
 
+// ---------------------------------------------------------
+// SPIKE PRIME (Cortex-M4F) - Hardware FPU Minimax
+// ---------------------------------------------------------
+#if defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
+
 static inline float pb_fast_sin(float theta) {
-    float x = theta;
-
-    // 1. Range Reduction to [-PI, PI]
-    #if defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
-    // Spike Prime (Cortex-M4F) optimized wrap
     float x_wrap = theta * 0.159154943f;
-    x = theta - (float)((int)(x_wrap + (x_wrap > 0.0f ? 0.5f : -0.5f))) * 6.2831853f;
-    #else
-    // EV3 (ARM9) stable wrap
-    while (x > 3.14159265f) {
-        x -= 6.28318531f;
-    }
-    while (x < -3.14159265f) {
-        x += 6.28318531f;
-    }
-    #endif
+    float x = theta - (float)((int)(x_wrap + (x_wrap > 0.0f ? 0.5f : -0.5f))) * 6.2831853f;
 
-    // 2. Mirroring to [-PI/2, PI/2]
     if (x > 1.5707963f) {
         x = 3.1415926f - x;
     } else if (x < -1.5707963f) {
         x = -3.1415926f - x;
     }
 
-    // 3. The Spike Minimax Polynomial
     float x2 = x * x;
     return x * (0.99999906f + x2 * (-0.16665554f + x2 * (0.00831190f + x2 * -0.00018488f)));
 }
@@ -37,4 +26,22 @@ static inline float pb_fast_cos(float theta) {
     return pb_fast_sin(theta + 1.57079633f);
 }
 
+// ---------------------------------------------------------
+// EV3 (ARM9) - glibc `<math.h>` optimized fallbacks
+// ---------------------------------------------------------
+#else
+
+static inline float pb_fast_sin(float theta) {
+    while (theta > 3.14159265f) theta -= 6.28318531f;
+    while (theta < -3.14159265f) theta += 6.28318531f;
+    return sinf(theta);
+}
+
+static inline float pb_fast_cos(float theta) {
+    while (theta > 3.14159265f) theta -= 6.28318531f;
+    while (theta < -3.14159265f) theta += 6.28318531f;
+    return cosf(theta);
+}
+
+#endif
 #endif // PYBRICKS_PLATFORM_MATH_H
